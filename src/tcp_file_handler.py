5ea32865838c0e631d256e294ad4b82cc03ad402
pawel <pawel@local.host>
2019-05-30 12:22:28 +0000

    exception superclass fixes handling and warning

--- src/tcp_file_handler.py
+++ src/tcp_file_handler.py
@@ -1,8 +1,15 @@
-
-from tcp_file_entry import EntryTCP
-from tcp_structs_exceptions import EntryTCP_FormatError
+"""
+    File parser and syntax analysis class, for /proc/net/tcp.
+"""
+from src.tcp_file_entry import EntryTCP
+from src.tcp_structs_exceptions import  EntryTCP_Error,\
+                                        EntryTCP_FormatError, \
+                                        FileTCP_InitError
 
 class FileTCP(object):
+    """
+        File data parser.
+    """
     def __init__(self, path):
         self.path = path
         self.data = None
@@ -13,8 +20,13 @@ class FileTCP(object):
             raise FileTCP_InitError("Not a valid path string: ", "{0}".format(path))
 
     def parse_entries(self):
-
-        if self.data == None:
+        """
+            If self.data is populated with the /net/tcp text file's read data,
+            it is analyzed by parse_entries for syntax checking, and passing
+            lines will be converted into EntryTCP objects and stored in object
+            self.entries.
+        """
+        if self.data is None:
             raise FileTCP_InitError("Fatal error, no data to parse.", "Tried with path: {0}".format(self.path))
 
         self.entries = self.data.split("\n")
@@ -24,7 +36,7 @@ class FileTCP(object):
             done = True
             for index in range(len(self.entries)):
                 if not isinstance(self.entries[index], EntryTCP):
-                    if type(self.entries[index]) is not str or len(self.entries[index]) == 0:
+                    if type(self.entries[index]) is not str or self.entries[index] == "":
                         # these lines do not pass to EntryTCP constructor
                         self.removed_lines += 1
                         del self.entries[index]
@@ -33,8 +45,7 @@ class FileTCP(object):
 
                     try:
                         self.entries[index] = EntryTCP(self.entries[index])
-                    #except EntryTCP_FormatError as fe:
-                    except:
+                    except EntryTCP_Error:
                         # lines with incorrect format are removed
                         self.removed_lines += 1
                         del self.entries[index]
