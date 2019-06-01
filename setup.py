7aa000aad1032bcc1991b9502c52a82f219913b6
pawel <pawel@local.host>
2019-06-01 14:01:55 +0000

    develop branch, packaging initialized

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='distnet',
+    version='0.1.0',
+    author='pawel',
+    author_email='pawel@local.host',
+    packages=['src'],
+    scripts=[''],
+    url='http://pypi.python.org/pypi/none',
+    license='LICENSE.txt',
+    description='Linux networking tools in python.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
