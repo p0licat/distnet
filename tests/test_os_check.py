4fee9ede833dad18f235ce3226b8adc697cdb278
pawel <pawel@local.host>
2019-06-20 05:55:26 +0000

    testing TravisCI networking

--- tests/test_os_check.py
+++ tests/test_os_check.py
@@ -18,7 +18,7 @@ def test_os_filesystem_check_empty():
 def test_os_filesystem_check_Passing():
     if "TRAVIS" in os.environ and os.environ["TRAVIS"] == "str_true":
 
-        assert os_filesystem_check(directory='chroot/proc/net///', files_list=[
+        assert os_filesystem_check(directory='tests/chroot/proc/net///', files_list=[
             '/tcp',
             '//tcp6',
             'udp',
@@ -38,7 +38,7 @@ def test_os_filesystem_check_not_existing():
     # is not dir
     if "TRAVIS" in os.environ and os.environ["TRAVIS"] == "str_true":
 
-        assert os_filesystem_check(directory='chroot/proc/exists/', files_list=[
+        assert os_filesystem_check(directory='tests/chroot/proc/exists/', files_list=[
             'tcp',
             'udp'
         ], filetype_pattern=': empty$') == False
@@ -55,7 +55,7 @@ def test_os_filesystem_check_not_existing():
 def test_os_filesystem_check_existing_file():
     # is not dir
     if "TRAVIS" in os.environ and os.environ["TRAVIS"] == "str_true":
-        assert os_filesystem_check(directory='chroot/proc/uptime', files_list=[
+        assert os_filesystem_check(directory='tests/chroot/proc/uptime', files_list=[
             'tcp',
             'udp'
         ], filetype_pattern=': empty$') == False
@@ -72,7 +72,7 @@ def test_os_filesystem_check_existing_file():
 def test_os_filesystem_check_existing_files_and_dirs():
     if "TRAVIS" in os.environ and os.environ["TRAVIS"] == "str_true":
 
-        assert os_filesystem_check(directory='chroot/proc/net', files_list=[
+        assert os_filesystem_check(directory='tests/chroot/proc/net', files_list=[
             'tcp12',    # should not exist
             'udp12',    # should not exist
             'stat'      # stat is a dir under /proc/net
